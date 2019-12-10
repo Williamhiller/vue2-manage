943ff2f39f5007de01191b6c9041482877314e70
Williamhiller <1747768963@qq.com>
2019-12-10 20:32:15 +0800

    test

--- test.js
+++ test.js
@@ -5,13 +5,16 @@ var phInstance = null;
 phantom.create()
     .then(instance => {
         phInstance = instance;
+        console.log(111111)
         return instance.createPage();
     })
     .then(page => {
         sitepage = page;
+        console.log(222222)
         return page.open('https://www.baidu.com/');
     })
     .then(status => {
+        console.log(333333)
         console.log(status);
         return sitepage.property('content');
     })
