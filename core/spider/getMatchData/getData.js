f93bed89eec9720ea3dff265c3c4f6c2e4f45f0c
Williamhiller <1747768963@qq.com>
2019-12-10 17:19:22 +0800

    断电

--- core/spider/getMatchData/getData.js
+++ core/spider/getMatchData/getData.js
@@ -16,8 +16,10 @@ module.exports = function (url) {
         _page = page;
         _page.setting('userAgent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3440.75 Safari/537.36');
 
+        console.log(_url)
         return _page.open(_url);
     }).then(function (status) {
+        console.log(status)
 
         return _page.evaluate(function () {
             var data = {};
@@ -42,6 +44,7 @@ module.exports = function (url) {
         _page.close();
         _ph.exit(0);
     }).catch(function (e) {
+        console.log(e)
         deferred.reject(e);
     });
 
