c12bd818ca2cb8e5fbaf98624d9544f796e642ab
Williamhiller <1747768963@qq.com>
2019-12-11 17:09:33 +0800

    // a phantomjs example var page = require('webpage').create(); phantom.outputEncoding="gbk"; page.open("http://www.topfe.cn", function(status) { if ( status === "success" ) { console.log(page.title); } else { console.log("Page failed to load."); } phantom.exit(0); });

--- test.js
+++ test.js
@@ -1,29 +1,11 @@
-var phantom = require('phantom');
-
-var sitepage = null;
-var phInstance = null;
-phantom.create()
-    .then(instance => {
-        phInstance = instance;
-        console.log(111111)
-        return instance.createPage();
-    })
-    .then(page => {
-        sitepage = page;
-        console.log(222222)
-        return page.open('https://www.baidu.com/');
-    })
-    .then(status => {
-        console.log(333333)
-        console.log(status);
-        return sitepage.property('content');
-    })
-    .then(content => {
-        console.log(content);
-        sitepage.close();
-        phInstance.exit();
-    })
-    .catch(error => {
-        console.log(error);
-        phInstance.exit();
-    });
+// a phantomjs example
+var page = require('webpage').create();
+phantom.outputEncoding="gbk";
+page.open("http://www.topfe.cn", function(status) {
+    if ( status === "success" ) {
+        console.log(page.title);
+    } else {
+        console.log("Page failed to load.");
+    }
+    phantom.exit(0);
+});
